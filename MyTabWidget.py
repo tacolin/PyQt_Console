291ef662decf2a1b9a1b87c2b9da7c7aa133fe86
tacolin <potegrant@yahoo.com.tw>
2014-07-25 18:19:41 +0800

    加了 ctrl + w 會 close tab 的功能

--- MyTabWidget.py
+++ MyTabWidget.py
@@ -22,11 +22,24 @@ class MyTabWidget(QTabWidget):
     self.setTabsClosable(True)
     self.tabCloseRequested.connect(self.closeTab)
 
+    closeTabShortcut = QShortcut(QKeySequence('Ctrl+W'), self)
+    closeTabShortcut.activated.connect(self.closeFocusedTab)
+
   # 為了讓 tab 上的叉叉按鈕，按下去以後真的會把 tab 關掉所做的
   @pyqtSlot(int)
-  def closeTab(self,tabIndex):
-    target = self.widget(tabIndex)
-    self.removeTab(tabIndex)
-    target.deleteLater()
+  def closeTab(self,tabIndex):    
+    if tabIndex < 0: # tabIndex 最小是 0
+      return
 
-    
+    target = self.widget(tabIndex)
+    self.removeTab(tabIndex)    
+    target.myClose() # 自己包過的 tab 內 widget，都要有一個 myClose 方法
+
+  @pyqtSlot()
+  def closeFocusedTab(self):
+    tabIndex = self.currentIndex()
+    if tabIndex >= 0: # tabIndex 最小是 0
+      self.closeTab(tabIndex)
+
+  def keyPressEvent(self,ev):
+    print '[tabWidget] key = {0}'.format(ev.key())
--- MyTextEdit.py
+++ MyTextEdit.py
@@ -10,3 +10,12 @@ class MyTextEdit(QTextEdit):
     super(MyTextEdit,self).__init__(parent)
 
     self.setReadOnly(True)
+
+  # 自己包過的 tab 內 widget，都要有一個 myClose 方法
+  def myClose(self):
+    self.deleteLater()
+
+  def keyPressEvent(self,ev):
+    print '[textEdit] key = {0}'.format(ev.key())
+
+    # super(MyTextEdit,self).keyPressEvent(ev)
