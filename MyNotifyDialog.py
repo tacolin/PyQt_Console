8069662c11b25f83ee2b42be6fb3cce4e4e48b95
tacolin <potegrant@yahoo.com.tw>
2014-07-28 01:44:30 +0800

    修改了一下 notify dialog

--- MyNotifyDialog.py
+++ MyNotifyDialog.py
@@ -5,51 +5,63 @@ from PyQt5.QtGui import *
 from PyQt5.QtWidgets import *
 
 
-class MyNotifyDialog:
-    @staticmethod
-    def info(string):
-        icon = QIcon('./icons/emoticon_smile.png')
+class MyNotifyDialog(QDialog):
+    def __init__(self, string, title, icon, buttonBoxType, parent=None):
+        super(MyNotifyDialog, self).__init__(parent)
         pixmap = icon.pixmap(48, 48)
 
-        dialog = QDialog()
-        dialog.setWindowTitle('INFO')
-        dialog.setFont(QFont('Arial', 14))
-        dialog.setWindowIcon(icon)
+        self.setWindowTitle(title)
+        self.setFont(QFont('Arial', 14))
+        self.setWindowIcon(icon)
 
-        picLabel = QLabel(dialog)
+        picLabel = QLabel(self)
         picLabel.setPixmap(pixmap)
         picLabel.setContentsMargins(0, 10, 10, 10)
 
-        textLabel = QLabel(string, dialog)
+        textLabel = QLabel(string, self)
         textLabel.setAlignment(Qt.AlignLeft | Qt.AlignVCenter)
         textLabel.setWordWrap(True)
 
-        infoWidget = QWidget(dialog)
+        infoWidget = QWidget(self)
         hlayout = QHBoxLayout(infoWidget)
         hlayout.addWidget(picLabel)
         hlayout.addWidget(textLabel)
         hlayout.addStretch()
 
-        buttonBox = QDialogButtonBox(QDialogButtonBox.Ok, dialog)
-        buttonBox.accepted.connect(dialog.accept)
+        buttonBox = QDialogButtonBox(buttonBoxType, self)
+        buttonBox.accepted.connect(self.accept)
+        buttonBox.rejected.connect(self.reject)
 
-        vlayout = QVBoxLayout(dialog)
+        vlayout = QVBoxLayout(self)
         vlayout.addWidget(infoWidget)
         vlayout.addWidget(buttonBox)
 
-        dialog.exec_()
+        self.setSizePolicy(QSizePolicy.Fixed, QSizePolicy.Fixed)
+
+    @staticmethod
+    def info(string):
+        icon = QIcon('./icons/emoticon_smile.png')
+        dialog = MyNotifyDialog(string, 'INFO', icon, QDialogButtonBox.Ok)
+        return dialog.exec_()
 
     @staticmethod
     def warn(string):
-        pass
+        icon = QIcon('./icons/emoticon_confused.png')
+        dialog = MyNotifyDialog(string, 'WARNING', icon, QDialogButtonBox.Ok)
+        return dialog.exec_()
 
     @staticmethod
     def error(string):
-        pass
+        icon = QIcon('./icons/emoticon_angry.png')
+        dialog = MyNotifyDialog(string, 'ERROR', icon, QDialogButtonBox.Ok)
+        return dialog.exec_()
 
     @staticmethod
     def confirm(string):
-        return True
+        icon = QIcon('./icons/help2.png')
+        dialog = MyNotifyDialog(string, 'CONFIRM', icon,
+                                QDialogButtonBox.Ok | QDialogButtonBox.Cancel)
+        return dialog.exec_()
 
 
 if __name__ == '__main__':
@@ -58,6 +70,14 @@ if __name__ == '__main__':
     signal.signal(signal.SIGINT, signal.SIG_DFL)
     app = QApplication(sys.argv)
 
-    print('開始')
-    MyNotifyDialog.info('當格數需要跨越多行時，就需要額外提供三個參數，例如 display 在第四橫列第一直行，因此第二個參數為 3 ，第三個參數為 0 ，第四個參數為垂直延伸格數，此處為 1 ，第五個參數為水平延伸格數，此處為 7')
-    print('結束')
+    print('INFO')
+    MyNotifyDialog.info('當格數需要跨越多行時，就需要額外提供三個參數，例如 display 在第四橫列第一直行，因此第二個參數為 3，第三個參數為 0 ，第四個參數為垂直延伸格數，此處為 1 ，第五個參數為水平延伸格數，此處為 7')
+    print('WARN')
+    MyNotifyDialog.warn('當格數需要跨越多行時，就需要額外提供三個參數，例如 display 在第四橫列第一直行，因此第二個參數為 3，第三個參數為 0 ，第四個參數為垂直延伸格數，此處為 1 ，第五個參數為水平延伸格數，此處為 7')
+    print('ERROR')
+    MyNotifyDialog.error('當格數需要跨越多行時，就需要額外提供三個參數，例如 display 在第四橫列第一直行，因此第二個參數為 3 ，第三個參數為 0 ，第四個參數為垂直延伸格數，此處為 1 ，第五個參數為水平延伸格數，此處為 7')
+    print('CONFIRM')
+    ret = MyNotifyDialog.confirm('當格數需要跨越多行時，就需要額外提供三個參數，例如 display 在第四橫列第一直行，因此第二個參數為 3 ，第三個參數為 0 ，第四個參數為垂直延伸格數，此處為 1 ，第五個參數為水平延伸格數，此處為 7')
+    print(ret)
+    ret = MyNotifyDialog.confirm('當格數需要跨越多行時，就需要額外提供三個參數，例如 display 在第四橫列第一直行，因此第二個參數為 3 ，第三個參數為 0 ，第四個參數為垂直延伸格數，此處為 1 ，第五個參數為水平延伸格數，此處為 7')
+    print(ret)
