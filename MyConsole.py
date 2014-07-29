b17e1a436ec655e4b6f76a9929e97af8c9ee0a55
tacolin <potegrant@yahoo.com.tw>
2014-07-29 19:57:22 +0800

    利用Qt serial port 重寫了一下 serial 部分

--- MyConsole.py
+++ MyConsole.py
@@ -1,89 +1,40 @@
 # -*- coding: utf-8 -*-
 
-from serial import *
-
 from PyQt5.QtCore import *
 from PyQt5.QtWidgets import *
 from PyQt5.QtGui import *
+from PyQt5.QtSerialPort import *
+import time
 
 
-class MyRecvEnd(Exception):
-    def __init__(self):
-        super(MyRecvEnd, self).__init__()
+class MySerialPort(QSerialPort):
+    def __init__(self, parent=None):
+        super(MySerialPort, self).__init__(parent)
 
 
-class MyRecv(QObject):
+def showAvailablePorts():
+    for port in QSerialPortInfo.availablePorts():
+        print('name {0}, desc {1}, serialNo. {2}, '
+              'location {3}, busy {4}'.format(port.portName(),
+                                              port.description(),
+                                              port.serialNumber(),
+                                              port.systemLocation(),
+                                              port.isBusy()
+                                              )
+              )
 
-    recvData = pyqtSignal(str)
+if __name__ == '__main__':
 
-    def __init__(self, serial, rxbuf):
-        super(MyRecv, self).__init__()
-        self.serial = serial
-        self.rxbuf = rxbuf
-        self.isTerminated = True
-        self.buf = ''
-        self.maxBufSize = 1000
-        self.checkList = []
+    serial = MySerialPort()
+    serial.setPortName('COM20')
+    serial.setBaudRate(QSerialPort.Baud115200)
+    serial.setFlowControl(QSerialPort.NoFlowControl)
 
-    @pyqtSlot()
-    def run(self):
-        self.isTerminated = False
-        firstByte = ''
-        restDataLen = 0
-        restBytes = ''
-        try:
-            while True and serial.isOpen():
-                firstByte = serial.read()
-                restDataLen = serial.inWaiting()
-                if restDataLen > 0:
-                    restBytes = serial.read(restDataLen)
-                else:
-                    restBytes = ''
-                self.notifyRecvData(firstByte, restBytes)
-                self.buf = ''.join([self.buf, firstByte, restBytes])
-                self.fitBuf()
-        except MyRecvEnd:
-            self.isTerminated = True
-            if self.serial.isOpen():
-                self.serial.Close()
-            self.buf = ''
+    if serial.open(QIODevice.ReadWrite) is True:
+        print('開啟成功')
+        serial.close()
+        print('關閉')
+    else:
+        print('開啟失敗, 原因:{0}'.format(serial.error()))
 
-    @pyqtSlot()
-    def terminate(self):
-        raise MyRecvEnd()
-
-    def fitBuf(self):
-        diff = len(self.buf) - self.maxBufSize
-        if diff > 0:
-            self.buf = self.buf[diff:]
-
-    def notifyRecvData(self, *datas):
-        for data in datas:
-            self.recvData.emit(data)
-
-    def checkBuf(self):
-        for wait in self.checkList:
-            pos = self.buf.find(wait['string'])
-            if pos != -1:
-                sig = pyqtSignal(bool)
-                sig.connect(wait['slot'])
-                sig.emit(True)
-                pos = pos + len(wait['string'])
-                self.buf = self.buf[pos:]
-
-    def addWaitString(self, string, slot, timeout):
-        self.checkList.append({'string': string, 'slot': slot})
-
-    def isTerminated(self):
-        return self.isTerminated
-
-
-class MyConsole(QObject):
-    def __init__(self, portName, textBrowser, waitTimeout=5):
-        super(MyConsole, self).__init()
-        self.portName = portName
-        self.textBrowser = textBrowser
-        try:
-            self.serial = Serial(None, baudrate=115200, timeout=waitTimeout)
-        except SerialException, e:
-            raise e
+    print('去你的')
